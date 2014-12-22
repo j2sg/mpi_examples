81cbb548184846238b12c67b6954ba51d9e13356
Juan José Salazar García <jjslzgc@gmail.com>
2014-12-22 13:48:09 +0100

    Added license information

--- hello/hello.c
+++ hello/hello.c
@@ -1,10 +1,30 @@
-#include <stdio.h>
-#include <mpi.h>
+/**
+ *  This file is part of MPI_Examples.
+ *
+ *  Copyright (c) 2014 Juan Jose Salazar Garcia jjslzgc@gmail.com
+ *
+ *  MPI_Examples is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MPI_Examples is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MPI_Examples.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
 
 /*
  *   Build: mpicc -o hello hello.c
  *   Run:   mpirun -np <Number of processors> hello
  */
+
+#include <stdio.h>
+#include <mpi.h>
 
 int main(int argc, char *argv[])
 {
--- send_recv/send_recv.c
+++ send_recv/send_recv.c
@@ -1,13 +1,34 @@
+/**
+ *  This file is part of MPI_Examples.
+ *
+ *  Copyright (c) 2014 Juan Jose Salazar Garcia jjslzgc@gmail.com
+ *
+ *  MPI_Examples is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MPI_Examples is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MPI_Examples.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+/*
+ *   Build: mpicc -o send_recv send_recv.c
+ *   Run:   mpirun -np <Number of processors> send_recv
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <mpi.h>
 
 #define MAXSTRING 128
 
-/*
- *   Build: mpicc -o send_recv send_recv.c
- *   Run:   mpirun -np <Number of processors> send_recv
- */
 
 int main(int argc, char *argv[])
 {
